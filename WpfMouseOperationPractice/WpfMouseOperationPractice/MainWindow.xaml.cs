e1059340463ba113df9fb8138c6535b615397cd8
むずでょ <muzudho1@gmail.com>
2021-09-18 15:20:00 +0900

    PointToScreen

--- WpfMouseOperationPractice/WpfMouseOperationPractice/MainWindow.xaml.cs
+++ WpfMouseOperationPractice/WpfMouseOperationPractice/MainWindow.xaml.cs
@@ -3,6 +3,7 @@
     using System.Diagnostics;
     using System.Windows;
     using System.Windows.Input;
+    using System.Windows.Media;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -16,12 +17,18 @@
 
         private void Window_MouseMove(object sender, MouseEventArgs e)
         {
-            Trace.WriteLine($"Window_MouseMove x={e.GetPosition((IInputElement)sender).X}");
+            // Trace.WriteLine($"Window_MouseMove x={e.GetPosition((IInputElement)sender).X:0.0f}");
+
+            var coord = ((Visual)sender).PointToScreen(e.GetPosition((IInputElement)sender));
+            Trace.WriteLine($"Window_MouseMove scx={coord.X:0.0f} scy={coord.Y:0.0f}");
         }
 
         private void Ellipse_MouseMove(object sender, MouseEventArgs e)
         {
-            Trace.WriteLine($"Ellipse_MouseMove x={e.GetPosition((IInputElement)sender).X}");
+            // Trace.WriteLine($"Ellipse_MouseMove x={e.GetPosition((IInputElement)sender).X:0.0f}");
+
+            var coord = ((Visual)sender).PointToScreen(e.GetPosition((IInputElement)sender));
+            Trace.WriteLine($"Ellipse_MouseMove scx={coord.X:0.0f} scy={coord.Y:0.0f}");
         }
     }
 }
